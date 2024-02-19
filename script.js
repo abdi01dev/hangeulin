aaa39d74e6defc3cc25b8b7969b74bb7a3f3361c
abdi01dev <abdi01dev@gmail.com>
2024-02-20 00:46:39 +0700

    add: menambahkan navbar

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ window.addEventListener('keyup', e => {
     document.body.classList.toggle('debug');
   }
 });
+
+// Navbar Toggle
+const navbarToggle = document.querySelector('.navbar__toggle');
+const navbar = document.querySelector('nav.navbar');
+
+navbarToggle.addEventListener('click', () => {
+  navbarToggle.classList.toggle('active');
+  navbar.classList.toggle('active');
+});
